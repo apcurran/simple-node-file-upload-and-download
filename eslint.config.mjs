e3b07695c904b9fd24cc9469699530ee42709dde
Alex Curran <alexcurran.va@gmail.com>
2025-02-12 16:49:53 -0600

    Add stylistic package to handling auto-formatting with ESLint in a valid way

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,27 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import stylistic from "@stylistic/eslint-plugin";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
+    {
+        files: ["**/*.js",], languageOptions: { sourceType: "commonjs", },
+    },
+    {
+        languageOptions: {
+            globals: globals.node,
+        },
+        plugins: {
+            "@stylistic": stylistic,
+        },
+        rules: {
+            "@stylistic/indent": ["error", 4,],
+            "@stylistic/semi": "error",
+            "@stylistic/arrow-parens": ["error", "always",],
+            "@stylistic/quotes": ["error", "double",],
+            "@stylistic/brace-style": "error",
+            "@stylistic/comma-dangle": ["error", "always",],
+        },
+    },
+    pluginJs.configs.recommended,
 ];
