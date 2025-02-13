b1e19e2e106d7236b02d1406ae7a4b733efb4aa2
Alex Curran <alexcurran.va@gmail.com>
2025-02-13 16:15:27 -0600

    Basic server setup for Express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);    
+});
