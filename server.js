dc37fa622936d7f4cb28214a93cf2c31bab729e8
Alex Curran <alexcurran.va@gmail.com>
2025-02-18 16:49:39 -0600

    Add morgan package for dev logging on server requests

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 const express = require("express");
 
-// routers
-const filesRouter = require("./routes/files-router");
-
 const app = express();
 const PORT = 5000;
+
+if (process.env.NODE_ENV === "development") {
+    const morgan = require("morgan");
+    app.use(morgan("dev"));
+}
+
+// routers
+const filesRouter = require("./routes/files-router");
 
 // enable API router
 app.use("/api", filesRouter);
