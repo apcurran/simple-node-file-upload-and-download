eb3c9fd312f6fbfef7d40571561ce215bf249042
Alex Curran <alexcurran.va@gmail.com>
2025-02-20 16:14:29 -0600

    Add upload.single() func to route for file uploads POST req

--- routes/files-router.js
+++ routes/files-router.js
@@ -3,10 +3,13 @@
 const express = require("express");
 
 const filesController = require("../controllers/files-controller");
+const { upload } = require("../config/multer-config");
 
 const router = express.Router();
 
-router.post("/upload-file", filesController.postUploadFile);
+// the 'name' field on the HTML form should match the upload function name
+// upload func will fail without matching names
+router.post("/upload-file", upload.single("file"), filesController.postUploadFile);
 
 router.get("/download-file/:file-name", filesController.getDownloadFile);
 
