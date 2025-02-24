1fa32f72b4f5b0aa4b5098ea0fd7ce95a398a97c
Alex Curran <alexcurran.va@gmail.com>
2025-02-24 16:16:52 -0600

    Refactor endpoints for better RESTful compliance

--- routes/files-router.js
+++ routes/files-router.js
@@ -7,9 +7,11 @@ const { upload } = require("../config/multer-config");
 
 const router = express.Router();
 
+router.get("/files", filesController.getUploadedFiles);
+
 // the 'name' field on the HTML form should match the upload function name
 // upload func will fail without matching names
-router.post("/upload-file", upload.single("file"), filesController.postUploadFile);
+router.post("/files", upload.single("file"), filesController.postUploadFile);
 
 router.get("/download-file/:file-name", filesController.getDownloadFile);
 
