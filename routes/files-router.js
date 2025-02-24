99e0b085d01ac5dadca049cabc2285dd6e2e1063
Alex Curran <alexcurran.va@gmail.com>
2025-02-24 16:18:54 -0600

    Refactor name for GET download file

--- routes/files-router.js
+++ routes/files-router.js
@@ -8,11 +8,10 @@ const { upload } = require("../config/multer-config");
 const router = express.Router();
 
 router.get("/files", filesController.getUploadedFiles);
-
 // the 'name' field on the HTML form should match the upload function name
 // upload func will fail without matching names
 router.post("/files", upload.single("file"), filesController.postUploadFile);
 
-router.get("/download-file/:file-name", filesController.getDownloadFile);
+router.get("/files/:filename", filesController.getDownloadFile);
 
 module.exports = router;
