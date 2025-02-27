461ae287c572f49da1d44d041f251569a818e982
Alex Curran <alexcurran.va@gmail.com>
2025-02-27 16:33:20 -0600

    GET /api/files route test

--- tests/files.test.js
+++ tests/files.test.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const request = require("supertest");
 const { describe, it } = require("node:test");
+const assert = require("assert");
 
 const filesRouter = require("../routes/files-router");
 
@@ -13,8 +14,15 @@ app.use(express.json());
 // router middleware
 app.use("/api", filesRouter);
 
-describe("Files routes integration tests", () => {
-    it("GET /api/files -- should return a JSON list of all uploaded files in the file-uploads dir", () => {
-        
+describe("GET /api/files", () => {
+    it("respond with a JSON list of all uploaded files in the file-uploads dir", (done) => {
+        request(app)
+            .get("/api/files")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect((response) => {
+                assert.ok(Array.isArray(response.body), "Response should be an array");
+            })
+            .expect(200, done);
     });
 });
